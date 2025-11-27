3f2d184d70210c12f8f104a5bcfd5c81a823ba71
Ian Weaver <weaveric@gmail.com>
2025-11-26 20:30:54 -0800

    dust-exctinction: Example 2 plots up

--- src/tutorials/dust-extinction.jl
+++ src/tutorials/dust-extinction.jl
@@ -21,19 +21,18 @@ begin
 	using DustExtinction
 	
 	# Data handling
-	using DataFramesMeta: DataFrame, @rsubset
+	using DataFramesMeta: DataFrame, Not, @rsubset
 	using VirtualObservatory: execute, TAPService
 	using FITSFiles: fits, info
 	using Downloads: download
 	using CodecZlib: GzipDecompressor
 	
 	# Units
-	using Unitful: Unitful as U
 	using DynamicQuantities: DynamicQuantities, @u_str, @us_str
 	using DynamicQuantities.Constants: c as c0
 
 	# Plots
-	using Makie: DQConversion
+	using Makie: DQConversion, IntervalsBetween
 	using CairoMakie: Figure, Axis, axislegend, lines, lines!, scatter!, ylims!
 	using LaTeXStrings: @L_str
 	using MathTeXEngine: set_texfont_family!, FontFamily
@@ -43,6 +42,9 @@ end;
 # ╔═╡ 89366506-4854-4859-b109-2d6cd9456bb5
 # Upstreaming to DynamicQuantities.jl
 using DynamicQuantities: @register_unit
+
+# ╔═╡ e7266de8-ba60-45b3-8988-6143e7098b72
+using DynamicQuantities: ustrip
 
 # ╔═╡ 95d05163-b6d2-4a01-a782-6da05479ee0a
 using PlutoUI: TableOfContents
@@ -97,24 +99,35 @@ md"""
 ## Example 1: Investigate Extinction Models
 """
 
+# ╔═╡ ed46db90-9802-4615-928f-bcad4bd19804
+md"""
+!!! todo
+	Add DQ support to DustExtinction.jl
+"""
+
 # ╔═╡ 0250218a-6670-483f-8122-045ca65d28e4
-wav = (0.1 : 0.001 : 3.0) * U.u"μm"
+# Avoid floating point issue right at boundary
+wav = let
+	ϵ = eps()
+	( (0.1 + ϵ) : 0.001 : 3.0 )u"μm"
+end
 
 # ╔═╡ eb1c1b16-c391-45a5-81c2-5fd3dbb12c50
 let
 	fig = Figure()
 	
 	ax = Axis(fig[1, 1];
+		dim1_conversion = DQConversion(u"1/μm"),
 		xlabel = L"\lambda^{-1} \left(\mu\text{m}^{-1}\right)",
-		ylabel = L"A(\lambda) / A(V)",
+		ylabel = "A(λ) / A(V)",
 	)
 
-	inv_wav = inv.(wav)
+	inv_wav = inv.(collect(wav))
 
 	for model in (CCM89, F99)
 		for Rv in (2.0, 3.0, 4.0)
 			ext = model(; Rv)
-			lines!(inv_wav, ext.(wav);
+			lines!(inv_wav, ext.(ustrip(u"Å", wav));
 				label = string(nameof(model), ", ", "R = ", Rv),
 			)
 		end
@@ -190,7 +203,7 @@ io = decompress_gz_to_iobuffer(fpath)
 # ╔═╡ 216027cd-7737-4056-9abc-d71c899f7568
 md"""
 !!! todo
-	Should something like this be moved to FITSFiles.jl?
+	Move decompression bit to FITSFiles.jl? Does something like this already exist in Base somewhere?
 """
 
 # ╔═╡ 03d97df3-1498-4c32-b33f-d7e8e7f26751
@@ -222,14 +235,23 @@ and store the wavelength and flux information into `wave_spectrum` and `flux_spe
 """
 
 # ╔═╡ ccb3ce4f-731e-433b-9479-773aa97ac0ce
-flux_spectrum = vec(t.data.FLUX)u"erg/s/Å/cm^2"
+wav_spectrum, flux_spectrum = let
+	flux_spectrum_raw = vec(t.data.FLUX)
 
-# ╔═╡ a4f58423-a263-4308-9074-85a46738c497
-wav_spectrum = range(;
-	start = first(t.data.WAVELENGTH),
-	step = first(t.data.DELTAW),
-	length = length(flux_spectrum),
-)u"Å"
+	wav_spectrum_raw = range(;
+		start = first(t.data.WAVELENGTH),
+		step = first(t.data.DELTAW),
+		length = length(flux_spectrum_raw),
+	)
+
+	# Remove spurious negative fluxes
+	idxs_bad = findall(<(0), flux_spectrum_raw)
+
+	(
+		wav_spectrum_raw[Not(idxs_bad)]u"Å", 
+		flux_spectrum_raw[Not(idxs_bad)]u"erg/s/cm^2/Å",
+	)
+end
 
 # ╔═╡ e92c118e-7547-4fbe-8a6b-9487beb99ff9
 md"""
@@ -258,11 +280,11 @@ Umag, Bmag, Vmag = first(phot_simbad)
 
 # ╔═╡ 1fac575d-9ff7-4fda-bae9-7dba874396fc
 md"""
-We next turn to converting these magnitudes to flux so that they can be plotted on the same scale as our spectrum.
+We need to convert these magnitudes to flux so that they can be plotted on the same scale as our spectrum.
 """
 
 # ╔═╡ 844446f9-88e1-468f-bcf2-ad638aaeb844
-# Central wavelengths
+# Central wavelengths, stored as an array for plotting later
 wav_phot = wav_U, wav_B, wav_V = [0.3660, 0.4400, 0.5530] .* u"μm"
 
 # ╔═╡ 101318a4-4bd2-4253-9e88-a814cbb6578b
@@ -296,18 +318,24 @@ flux_phot = [
 	to_flux(flux_V0, Vmag),
 ]
 
+# ╔═╡ 637830ec-1b5a-4eaa-bb76-55acd3b08985
+md"""
+!!! note
+	See [PhotometricFilters.jl](https://juliaastro.org/PhotometricFilters) for a comprehensive treatment of photometric filter curves.
+"""
+
 # ╔═╡ e59fa830-195f-45fe-bdc0-b4a2b23cd8c4
 md"""
-### Plot
+### Initial spectrum + photometry
 """
 
 # ╔═╡ 84386f76-3972-4985-9dc4-05501bbc5f41
-begin
+let
 	# Spectrum
 	fig, ax, p = lines(wav_spectrum, flux_spectrum;
 		axis = (;
 			dim1_conversion = DQConversion(us"Å"),
-			dim2_conversion = DQConversion(us"erg/s/Å/cm^2"),
+			dim2_conversion = DQConversion(us"erg/s/cm^2/Å"),
 			xlabel = "Wavelength",
 			ylabel = "Flux",
 			title = "ρ Oph",
@@ -321,7 +349,64 @@ begin
 
 	# Legend and axis limits
 	axislegend()
-	ylims!(ax, (0u"erg/s/Å/cm^2", (3e-10)u"erg/s/Å/cm^2"))
+	ylims!(ax, (0u"erg/s/cm^2/Å", (3.1e-10)u"erg/s/cm^2/Å"))
+	fig
+end
+
+# ╔═╡ dba6f2b7-890a-4e7d-a86c-02e99f985796
+md"""
+### Dereddened spectrum + photometry
+"""
+
+# ╔═╡ b1207731-73be-4390-a329-30f8c1e7bc94
+flux_spectrum_deredden = deredden.(
+	F99(; Rv = 5), ustrip.(u"Å", wav_spectrum), flux_spectrum;
+	Av = 5 * 0.5 # Aᵥ = Rᵥ * E(B - V)
+) .|> us"erg/s/cm^2/Å"
+
+# ╔═╡ 7f53c04c-5e51-4fb4-bd7c-64bc2b9d2944
+flux_phot_deredden = deredden.(
+	F99(; Rv = 5), ustrip.(u"Å", wav_phot), flux_phot;
+	Av = 5 * 0.5 # Aᵥ = Rᵥ * E(B - V)
+) .|> us"erg/s/cm^2/Å"
+
+# ╔═╡ ff040052-e0b3-4447-811e-4f743456aed3
+let
+	# Spectrum
+	fig, ax, p = lines(wav_spectrum, flux_spectrum_deredden;
+		axis = (;
+			dim1_conversion = DQConversion(us"Å"),
+			dim2_conversion = DQConversion(us"erg/s/cm^2/Å"; units_in_label = false),
+			yscale = log10,
+			yminorticksvisible = true,
+			yminorticks = IntervalsBetween(9),
+			xlabel = "Wavelength",
+			ylabel = "Flux (erg/s/cm^2/Å)",
+			title = "ρ Oph",
+		),
+		color = :magenta,
+		label = "IUE spectrum dereddened",
+	)
+	lines!(wav_spectrum, flux_spectrum;
+		color = :cyan,
+		label = "IUE spectrum",
+	)
+
+	# Photometry
+	scatter!(ax, wav_phot, flux_phot_deredden;
+		color = :darkmagenta,
+		markersize = 15,
+		label = "U, B, V dereddened",
+	)
+	scatter!(ax, wav_phot, flux_phot;
+		color = :darkcyan,
+		markersize = 15,
+		label = "U, B, V",
+	)
+
+	# Legend and axis limits
+	axislegend()
+	ylims!(ax, (1e-11u"erg/s/cm^2/Å", 5e-8u"erg/s/cm^2/Å"))
 	fig
 end
 
@@ -347,7 +432,6 @@ LaTeXStrings = "b964fa9f-0449-5b57-a5c2-d3ea65f4040f"
 Makie = "ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a"
 MathTeXEngine = "0a4f8689-d25c-4efe-a92b-7142dfc1aa53"
 PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
-Unitful = "1986cc42-f94f-5a68-af5c-568840ba703d"
 VirtualObservatory = "d7ce213e-d3b9-4ed1-b00e-1146b7ac83e0"
 
 [compat]
@@ -361,7 +445,6 @@ LaTeXStrings = "~1.4.0"
 Makie = "~0.24.7"
 MathTeXEngine = "~0.6.7"
 PlutoUI = "~0.7.75"
-Unitful = "~1.25.1"
 VirtualObservatory = "~0.1.14"
 """
 
@@ -371,7 +454,7 @@ PLUTO_MANIFEST_TOML_CONTENTS = """
 
 julia_version = "1.12.2"
 manifest_format = "2.0"
-project_hash = "30583adfd34f4e8ee51c61f88f0a8b461c7b2b6c"
+project_hash = "9a97c3587c2b76ccbbf58f314e087a368362976a"
 
 [[deps.AbstractFFTs]]
 deps = ["LinearAlgebra"]
@@ -2547,6 +2630,8 @@ version = "4.1.0+0"
 # ╠═89366506-4854-4859-b109-2d6cd9456bb5
 # ╠═555cf1bd-5b52-4d26-aaa2-033a9f4d831a
 # ╟─f3bf7784-b2da-4ed6-9cb5-0799d81d65ae
+# ╠═e7266de8-ba60-45b3-8988-6143e7098b72
+# ╟─ed46db90-9802-4615-928f-bcad4bd19804
 # ╠═0250218a-6670-483f-8122-045ca65d28e4
 # ╠═eb1c1b16-c391-45a5-81c2-5fd3dbb12c50
 # ╟─943661a3-3b40-4c01-a624-79726e56b385
@@ -2565,9 +2650,8 @@ version = "4.1.0+0"
 # ╟─207dca91-36b1-48fa-b917-918e6a1edebd
 # ╠═5759aefa-2e5f-4f96-afa7-897f5171082f
 # ╠═3e55d0af-a930-40da-b5ea-b822ce01140d
-# ╠═f97b20a0-235b-4efe-aad0-75733290dc0a
+# ╟─f97b20a0-235b-4efe-aad0-75733290dc0a
 # ╠═ccb3ce4f-731e-433b-9479-773aa97ac0ce
-# ╠═a4f58423-a263-4308-9074-85a46738c497
 # ╟─e92c118e-7547-4fbe-8a6b-9487beb99ff9
 # ╟─37f294a1-448d-478b-a593-bfeabb1c84f6
 # ╠═a439386d-2b9b-44e8-9c41-cb2ec787024e
@@ -2579,9 +2663,14 @@ version = "4.1.0+0"
 # ╠═4ee58446-1563-44c8-b31f-6f9c3722f707
 # ╠═1c4d9d17-ab51-4ecc-9caf-c068ffc65fd2
 # ╠═36f69f11-10d8-4d42-b587-2b885f497973
-# ╠═855cfb7b-5e66-415c-9d48-f66b38e835ff
+# ╟─855cfb7b-5e66-415c-9d48-f66b38e835ff
+# ╟─637830ec-1b5a-4eaa-bb76-55acd3b08985
 # ╟─e59fa830-195f-45fe-bdc0-b4a2b23cd8c4
 # ╠═84386f76-3972-4985-9dc4-05501bbc5f41
+# ╟─dba6f2b7-890a-4e7d-a86c-02e99f985796
+# ╠═b1207731-73be-4390-a329-30f8c1e7bc94
+# ╠═7f53c04c-5e51-4fb4-bd7c-64bc2b9d2944
+# ╠═ff040052-e0b3-4447-811e-4f743456aed3
 # ╟─8a2e5f54-ec9d-46c5-88c6-aa647e7ea036
 # ╠═95d05163-b6d2-4a01-a782-6da05479ee0a
 # ╠═8e118239-43e0-427c-bdf1-80c9d5fe130c
