793df0088aefee09aa6587efd1ce3e6fc6d42889
Ian Weaver <weaveric@gmail.com>
2025-12-04 13:45:35 -0800

    units-DQ: first exercise up

--- src/tutorials/units-DQ.jl
+++ src/tutorials/units-DQ.jl
@@ -16,7 +16,7 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 4d03db22-f125-4f40-a7d1-149e44d36aea
-using DynamicQuantities: @u_str, @us_str, dimension, ustrip
+using DynamicQuantities: @u_str, @us_str, dimension, uconvert, ustrip
 
 # ╔═╡ 33aa1ce6-b0f2-4dcc-b30a-9410a365c0af
 using DynamicQuantities.Constants: pc, G
@@ -28,10 +28,10 @@ using StatsBase: mean
 using Distributions: Normal
 
 # ╔═╡ f1607fa8-67a6-408b-82c0-b63e9e2531db
-using CairoMakie: hist
+using CairoMakie: stephist
 
 # ╔═╡ 17c6b7df-a8b3-45d1-9491-526afce11318
-using PlutoUI: TableOfContents
+using PlutoUI: TableOfContents, details
 
 # ╔═╡ c6ad0267-65d1-4372-a538-22acd9b5d02b
 md"""
@@ -64,26 +64,28 @@ md"""
 
 # ╔═╡ 60c89d86-942e-4c97-bd7a-ad2f792b1155
 md"""
-## Start
+## 1. Galaxy mass
 """
 
 # ╔═╡ 1d2293bd-a236-4b41-a9d7-9c27463b5062
-Reff = 29 * u"Constants.pc"
-
-# ╔═╡ 97ab9725-a873-49f5-bda2-8da3983d95d7
-29 * pc
-
-# ╔═╡ e215179a-6a7c-4764-a01a-1ab17aac01d0
-ustrip(Reff)
+Reff = 29 * u"Constants.pc" # Or 29 * pc
 
 # ╔═╡ 9b6e871f-0a49-4a24-a9b0-51e3008d6db4
+ustrip(Reff)
+
+# ╔═╡ 1b239188-bba1-44d1-bc9c-10b60f762e0d
 ustrip(pc, Reff)
 
 # ╔═╡ cecd879b-2d95-4150-8caf-c327165ddec6
 dimension(Reff)
 
+# ╔═╡ cc736aec-c48a-411a-af83-4255309d77e9
+md"""
+Furthermore, we can convert the radius to any other unit of length. Here, we convert it to kilometers:
+"""
+
 # ╔═╡ 2c7a71f6-e618-40db-80b7-1ad5f09e59d5
-Reff |> us"km"
+Reff |> us"km" # Or uconvert(us"km", Reff)
 
 # ╔═╡ c5b4f340-c774-4f09-af4c-f326afce5de3
 md"""
@@ -97,16 +99,16 @@ v = rand(Normal(206, 4.3), 500)u"km/s"
 first(v, 10) .|> us"km/s"
 
 # ╔═╡ b9264571-6acf-4bc9-bdd7-8a9a2528410e
-hist(ustrip(us"km/s", v))
+stephist(ustrip(us"km/s", v))
 
 # ╔═╡ d8a2219e-1094-4eb7-be34-2ce58b6bd462
 md"""
 !!! todo
-	See if units + histograms will work ootb with <https://github.com/MakieOrg/Makie.jl/pull/5323>
+	Units + histograms (and more!) working in <https://github.com/MakieOrg/Makie.jl/pull/5323>. Update once this PR is in.
 """
 
 # ╔═╡ 2d236d32-faf4-41ef-84de-1d40c02fb238
-sigma = (sqrt∘sum)((v .- mean(v)).^2 / length(v))
+sigma = (sqrt ∘ sum)((v .- mean(v)).^2 / length(v))
 
 # ╔═╡ 64bfb535-ab84-411c-9a1b-3ed8778e8516
 sigma |> us"km/s"
@@ -120,8 +122,69 @@ M .|> (us"Constants.M_sun", us"g")
 # ╔═╡ 9e13bde0-eccf-4d21-b603-fd186e87b7d0
 (log10 ∘ ustrip)(us"Constants.M_sun", M)
 
+# ╔═╡ 5a9dfdf7-aaf8-421d-a451-6a6a5c35e1cc
+md"""
+Note that this is different than:
+"""
+
+# ╔═╡ a13de611-145a-40da-9414-8f3f8f85ad98
+(log10 ∘ ustrip)(M)
+
+# ╔═╡ a3f42e7e-af38-46b0-b1fb-bdbf77312e16
+md"""
+emphasizing the importance of being explicit with our units. Similarly, taking the logarithm of something with units is not mathematically well defined, so this will sensibly error as well:
+"""
+
 # ╔═╡ 095e7efe-f4c6-4a8a-a029-03ce97cd15bd
-log10(M) # Should error
+log10(M)
+
+# ╔═╡ 2648b454-2762-4941-b13c-2303ffcd6521
+let
+	sol = details("Solution",
+	md"""
+	```julia
+	using DynamicQuantities: Constants as C
+	
+	# Speed from Kepler's law
+	v_kep = sqrt(C.G * C.M_sun / C.au)
+	
+	# View in units of km/s (≈ 29.7847 km/s)
+	v_kep |> us"km/s"
+	
+	# Speed from kinematics (≈ 29.7853 km/s)
+	v_kin = 2π * C.au / u"yr"
+	
+	# Percent difference (≈ 0.002%)
+	percent_diff = 100 * (v_kin - v_kep) / v_kep
+	```
+	""")
+	
+	md"""
+	!!! warn "Exercise"
+	
+		Use DynamicQuantities.jl and Kepler's law in the form given below to determine the (circular) orbital speed of the Earth around the Sun in km/s:
+		
+		```math
+		v = \sqrt{\frac{G M_⊙}{r}}
+		```
+	
+		No need to look up constants or conversion factors to do this calculation -- it's all in `DynamicQuantities.Units` and `DynamicQuantities.Constants`.
+	
+		There's a much easier way to figure out the velocity of the Earth using just two units or quantities. Do that and then compare to the Kepler's law answer (the easiest way is probably to compute the percentage difference, if any).
+
+		$(sol)
+
+		Completely optional, but a good way to convince ourselves of the value of DynamicQuantities.jl: Do the above calculations by hand. Look up all the appropriate conversion factors and use paper-and-pencil / basic approaches for keeping track of them all. Using Julia as a basic calculator is also fine. Which one took longer?
+	"""
+end
+
+# ╔═╡ 48fac714-ab96-4475-ad0b-0c61432bf849
+# Your code here
+
+# ╔═╡ 9e8eabc7-f1ce-4dbb-bfd5-e6d28793f9d6
+md"""
+## 2. Molecular cloud mass
+"""
 
 # ╔═╡ 59b4d441-9a74-468f-ad8c-882516a09049
 md"""
@@ -152,7 +215,7 @@ StatsBase = "~0.34.8"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.12.1"
+julia_version = "1.12.2"
 manifest_format = "2.0"
 project_hash = "5638ceb33c533f708cf33724059eba0b2f4bcafc"
 
@@ -441,7 +504,7 @@ version = "0.9.5"
 [[deps.Downloads]]
 deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
 uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
-version = "1.6.0"
+version = "1.7.0"
 
 [[deps.DynamicQuantities]]
 deps = ["DispatchDoctor", "PrecompileTools", "TestItems", "Tricks"]
@@ -907,7 +970,7 @@ version = "0.6.4"
 [[deps.LibCURL_jll]]
 deps = ["Artifacts", "LibSSH2_jll", "Libdl", "OpenSSL_jll", "Zlib_jll", "nghttp2_jll"]
 uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
-version = "8.11.1+1"
+version = "8.15.0+0"
 
 [[deps.LibGit2]]
 deps = ["LibGit2_jll", "NetworkOptions", "Printf", "SHA"]
@@ -1114,7 +1177,7 @@ version = "0.8.7+0"
 [[deps.OpenSSL_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "458c3c95-2e84-50aa-8efc-19380b2a3a95"
-version = "3.5.1+0"
+version = "3.5.4+0"
 
 [[deps.OpenSpecFun_jll]]
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "JLLWrappers", "Libdl"]
@@ -1746,9 +1809,9 @@ uuid = "1317d2d5-d96f-522e-a858-c73665f53c3e"
 version = "2022.0.0+1"
 
 [[deps.p7zip_jll]]
-deps = ["Artifacts", "Libdl"]
+deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
 uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
-version = "17.5.0+2"
+version = "17.7.0+0"
 
 [[deps.x264_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl"]
@@ -1769,16 +1832,16 @@ version = "4.1.0+0"
 # ╠═4d03db22-f125-4f40-a7d1-149e44d36aea
 # ╠═33aa1ce6-b0f2-4dcc-b30a-9410a365c0af
 # ╠═702a51d1-d040-4ee9-ae72-fc4f093133c9
-# ╟─60c89d86-942e-4c97-bd7a-ad2f792b1155
-# ╠═1d2293bd-a236-4b41-a9d7-9c27463b5062
-# ╠═97ab9725-a873-49f5-bda2-8da3983d95d7
-# ╠═e215179a-6a7c-4764-a01a-1ab17aac01d0
-# ╠═9b6e871f-0a49-4a24-a9b0-51e3008d6db4
-# ╠═cecd879b-2d95-4150-8caf-c327165ddec6
-# ╠═2c7a71f6-e618-40db-80b7-1ad5f09e59d5
-# ╟─c5b4f340-c774-4f09-af4c-f326afce5de3
 # ╠═5f91f3fb-d959-46d4-af7c-6e5626f7295a
 # ╠═f1607fa8-67a6-408b-82c0-b63e9e2531db
+# ╟─60c89d86-942e-4c97-bd7a-ad2f792b1155
+# ╠═1d2293bd-a236-4b41-a9d7-9c27463b5062
+# ╠═9b6e871f-0a49-4a24-a9b0-51e3008d6db4
+# ╠═1b239188-bba1-44d1-bc9c-10b60f762e0d
+# ╠═cecd879b-2d95-4150-8caf-c327165ddec6
+# ╟─cc736aec-c48a-411a-af83-4255309d77e9
+# ╠═2c7a71f6-e618-40db-80b7-1ad5f09e59d5
+# ╟─c5b4f340-c774-4f09-af4c-f326afce5de3
 # ╠═892f19a6-af21-4353-9987-9de795ba7ad7
 # ╠═808f338d-7faa-4d97-ad7b-a7b4066c83f8
 # ╠═b9264571-6acf-4bc9-bdd7-8a9a2528410e
@@ -1788,7 +1851,13 @@ version = "4.1.0+0"
 # ╠═a3baf049-5419-4773-8b7f-0ba07a9f1728
 # ╠═bb2622f9-98a5-48ca-92cb-bbce84d797f1
 # ╠═9e13bde0-eccf-4d21-b603-fd186e87b7d0
+# ╟─5a9dfdf7-aaf8-421d-a451-6a6a5c35e1cc
+# ╠═a13de611-145a-40da-9414-8f3f8f85ad98
+# ╟─a3f42e7e-af38-46b0-b1fb-bdbf77312e16
 # ╠═095e7efe-f4c6-4a8a-a029-03ce97cd15bd
+# ╟─2648b454-2762-4941-b13c-2303ffcd6521
+# ╠═48fac714-ab96-4475-ad0b-0c61432bf849
+# ╟─9e8eabc7-f1ce-4dbb-bfd5-e6d28793f9d6
 # ╟─59b4d441-9a74-468f-ad8c-882516a09049
 # ╠═17c6b7df-a8b3-45d1-9491-526afce11318
 # ╠═bedc8ccd-e6f6-4dd1-a0b6-1889f4b5b658
