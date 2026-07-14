9af9f3347715c37f916900a618e7e39b16fcb50b
Ian Weaver <weaveric@gmail.com>
2026-07-13 19:21:31 -0700

    more text

--- src/tutorials/fits-images.jl
+++ src/tutorials/fits-images.jl
@@ -69,11 +69,9 @@ begin
     # Data handling and visualization
     using DataFramesMeta: DataFrame, @rsubset!
     using Downloads: download
-    using FITSFiles: fits, info
     using Printf: @sprintf
     using AstroImages
-    using CairoMakie: Colorbar, IntervalsBetween, plot, stephist
-    # using AlgebraOfGraphics: data, mapping, histogram, visual, draw, scales
+    using CairoMakie
 
     deps_ready = true # Can remove this too after upstream merge
 end
@@ -91,118 +89,13 @@ end
 md"""
 ## Summary
 
-Following up from [Working with FITS tables](/tutorials/fits-tables/), this tutorial first demonstrates how to use [AstroImages.jl](https://juliaastro.org/AstroImages) to preview images formed from FITS data tables before using [Makie.jl](https://makie.org) + [AlgebraOfGraphics.jl](https://aog.makie.org) to make publication-ready plots. Next, we will demonstrate how to these tools to help visualize simple image stacking from FITS images and save it back to file.
+This tutorial demonstrates how to use [AstroImages.jl](https://juliaastro.org/AstroImages) to view image data from FITS files and create publication-ready visualizations with different color scales and stretches. We will also demonstrate how to perform simple image stacking.
 """
 
 # ╔═╡ a6e33cf8-1fe3-4810-a66b-adc07166871e
 md"""
 ### Packages 📦
 """
-
-# ╔═╡ f87da6ab-c718-47c3-bedc-78cd025b40e6
-md"""
-## Images from FITS tables
-"""
-
-# ╔═╡ 3b974213-d2a5-4528-b27b-d4c608319213
-md"""
-### Load data
-
-We start by loading in the data from the previous tutorial. We will use AstroImages.jl, which calls to FITSFiles.jl under the hood, similarly to the previous tutorial:
-"""
-
-# ╔═╡ bfbf0366-1115-4e96-beb1-0b012dbd8a18
-hdus_chandra = let
-    fname = download(
-        "http://data.astropy.org/tutorials/FITS-tables/chandra_events.fits"
-    )
-    load(fname, :)
-end
-
-# ╔═╡ 2472e2a8-27b6-4f18-9dce-d0ef12d6c257
-df_evt_main = let
-    df = DataFrame(hdus_chandra[2])
-    @rsubset! df :ccd_id ∈ 0:3
-end
-
-# ╔═╡ ae04bf64-0ea2-4da2-b6ae-a86f11a786b8
-md"""
-!!! note
-    We use the mutating version of `@rsubset` (note the exclamation mark above) because we do not need to preserve the original `DataFrame`, in this case. For more on mutating functions, see [this section](https://docs.julialang.org/en/v1/manual/variables/#man-assignment-expressions) of the Julia manual.
-
-We next look at a few different ways that we can visualize the 2D histogram that we roughly previewed in the previous tutorial; from explicit (less convenient) to implicit (more convenient).
-"""
-
-# ╔═╡ c26d8744-0026-4f43-b056-1c94ee958f8b
-md"""
-### View with AstroImages.jl
-
-[AstroImages.jl](https://juliaastro.org/AstroImages/) is the main starting point in the JuliaAstro ecosystem for easily loading and viewing FITS image files.
-
-We start by using `StatsBase.fit` and `StatsBase.Histogram` to bin the ``x`` and ``y`` data ourselves before passing it to `imview` to view the image directly in the notebook. Later we will see how to do this binning step automatically for us:
-"""
-
-# ╔═╡ 430611bb-6caf-4b38-837d-fc05173d4f00
-h = fit(Histogram, (df_evt_main.x, df_evt_main.y); nbins = 400)
-
-# ╔═╡ 515e570b-d140-4d66-845e-cae7b4cecd05
-md"""
-The weights are returned as an `AbstractArray`, which can be viewed directly with AstroImages.jl:
-"""
-
-# ╔═╡ bb11ae83-b919-4511-86cf-51baf77d89a3
-imview(
-    h.weights;
-    # clims = Percent(99.5),
-    # stretch = identity,
-    cmap = :cividis,
-    # contrast = 1.0,
-    # bias = 0.5,
-)
-
-# ╔═╡ 7401caa5-c891-4306-bb3c-a4a9cab7012b
-md"""
-We can immediately see structure appear in our image, and the outlines of the four main (ACIS-I) chips. Try adjusting the imaging options commented out above to modify the image. See the `imview` documentation in the Live docs of this notebook for all available options.
-
-!!! tip
-    Try passing your own AbstractArray to `imview`. This function converts an array of numbers to an array of `ColorTypes` (i.e., RGB values) for visualizing our arrays in full color. For more on this, see the [Arrays, Numbers, and Colors](https://juliaimages.org/latest/tutorials/arrays_colors/#page_arrays_colors) section of the JuliaImages documentation.
-"""
-
-# ╔═╡ d031ddf9-ee84-46b2-b387-1ec99a2ca79b
-# Your code here, e.g., imview(rand(3, 4))
-
-# ╔═╡ c4374601-9c65-4bd2-b2c0-3e83597395bf
-md"""
-Next we will see how to plot this image with labeled axes and a properly formatted colorbar.
-"""
-
-# ╔═╡ 5dd43020-71e9-47e7-9b57-295e57a98bce
-md"""
-### Plotting with Makie.jl
-
-Makie.jl is a modern plotting ecosystem written in pure Julia. Its [set of backends](https://docs.makie.org/stable/explanations/backends/backends#What-is-a-backend) allows us to produce plots for a wide range of contexts. For this tutorial, we will use the CairoMakie.jl backend to produce publication-quality vector graphic plots.
-
-To start, we will pass the histogram object `h` directly to Makie, which its `plot` command knows how to handle:
-"""
-
-# ╔═╡ 02eaf214-7238-45b3-9674-c1b7f1b7d10e
-let
-    fig, ax, p = plot(
-        h.weights;
-        colorrange = (1, 10_000),
-        colorscale = log10,
-        colormap = :cividis,
-    )
-
-    Colorbar(
-        fig[1, 2], p;
-        ticks = [1, 3, 6, 500, 10_000],
-        minorticksvisible = true,
-        minorticks = IntervalsBetween(9),
-    )
-
-    fig
-end
 
 # ╔═╡ 74f155a2-fe8a-404f-b0c6-7a4f2724c408
 md"""
@@ -212,93 +105,58 @@ md"""
 You may notice that there are still a few things missing from our plot that would be nice to have by default, e.g., labeled axes and a formatted colorbar. We will show an ergonomic way to do this next.
 """
 
-# ╔═╡ 88f9fe70-44d0-469c-8472-8def72b7c3d0
-md"""
-## AstroImages recipe
-"""
-
-# ╔═╡ f667c0c9-b52c-4fca-9a74-f967c25279d8
-implotview(h.weights, cmap = :cividis)
-
-# ╔═╡ 414b0415-e784-4089-a683-a20ec15ee44f
-md"""
-### Plotting with Makie.jl + AoG.jl
-
-Similarly to seaborn for Python, or ggplot2 in R, Julia provides AlgebraOfGraphics.jl, a plotting framework that extends existing plotting capabilities for a wide range of statistical and visualization applications of structured data.
-
-As with these other packages, its usecases are probably best shown by example:
-"""
-
-# ╔═╡ 235242b0-a8c6-4627-9b2c-fc14f705c686
-# let
-#     plt = data(df_evt_main) * mapping(:x, :y) * histogram(bins = 400)
-
-#     draw(
-#         plt, scales(
-#             Color = (colorrange = (1, 10_000), scale = log10, colormap = :cividis)
-#         );
-#         colorbar = (
-#             ticks = [1, 3, 6, 500, 10_000],
-#             minorticksvisible = true,
-#             minorticks = IntervalsBetween(9),
-#         )
-#     )
-# end
-
-# ╔═╡ 5ea5f0ee-ae78-4f35-8900-011416c7d508
-md"""
-!!! todo
-    AoG + Make v0.25 support
-"""
-
-# ╔═╡ f7aca318-a535-4a86-8c29-92d90db96271
-md"""
-Here, we reproduce the previous plot above, but now with the desired axes labeling and colorbar formatting applied for us. The colorbar is also automatically labeled for us and the shape of its endpoints are adjusted to triangles to show that the data values extend beyond what is shown on the colorbar scale.
-
-Additionally, note that we are working directly with the DataFrame object `df_evt-main` now instead of needing to manually fit a histogram beforehand.
-
-!!! tip
-    See this very nice [tutorial series](https://aog.makie.org/stable/tutorials/intro-i) in the AlgebraOfGraphics.jl documentation for more.
-
-We now turn to working directly with FITS image data.
-"""
-
 # ╔═╡ 0598bfc5-0900-4b04-ad84-9638b89869b9
 md"""
-## Images from FITS arrays
+## Opening FITS files and viewing the image data
 
-For the rest of this tutorial, we will work with an astronomical image of the Horsehead Nebula taken with a photographic plate. The image has been digitized, i.e., scanned by a computer and converted to a 2D array. Each position in the array corresponds with the projected position on the sky, and bright areas of the image have high values while dark areas have low values in the array.
+For this tutorial, we will work with an astronomical image of the Horsehead Nebula taken with a photographic plate. The image has been digitized, i.e., scanned by a computer and converted to a 2D array. Each position in the array corresponds with the projected position on the sky, and bright areas of the image have high values while dark areas have low values in the array.
 
 Images taken with astronomical instruments called CCDs or "[charge-coupled devices](https://en.wikipedia.org/wiki/Charge-coupled_device)" are organized similarly. When illuminated by light, CCDs accumulate electrons, converting brightness values to electron counts. A CCD image is essentially a 2D array, where each position on the array represents a single CCD pixel, and the values in that array represent the number of counts registered in that pixel.
 """
 
-# ╔═╡ ad35dd30-40f5-4bd7-b1d6-b81a226e85ab
+# ╔═╡ 622ae936-36d2-4154-a82b-e8fe33773f92
+img_file = download("http://data.astropy.org/tutorials/FITS-images/HorseHead.fits");
+
+# ╔═╡ 3b813a3a-b24d-4801-bd1a-d7ac3106518b
+img = load(img_file)
+
+# ╔═╡ d9881179-49b8-40af-8aa0-233d7bb1496c
 md"""
-### Load data
+!!! tip
+     If you are in an interactive environment like VSCode, Jupyter, or Pluto (this tutorial), instead of a REPL, AstroImages are automatically rendered to images and displayed.
 
-We start by downloading our data of the Horsehead Nebula from the link below:
+!!! todo
+    Forward FITSFiles `fits` kwargs like `scale` from `AstroImages/load` to preserve the original data type specified in the `BITPIX` header card
 """
-
-# ╔═╡ 1e05c329-a82a-4ed8-ba02-11e155539059
-hdus = let
-    fpath = download("http://data.astropy.org/tutorials/FITS-images/HorseHead.fits")
-    fits(fpath; scale = false)
-end
-
-# ╔═╡ eaf39dba-e724-4eac-9a14-28fcd179efc7
-md"""
-Generally, images are stored in the `PRIMARY` block. Let's take a look at it:
-"""
-
-# ╔═╡ 798dbe98-c2d5-4dd2-b624-f5df795ef470
-img_data = hdus[1].data
 
 # ╔═╡ a9ec676e-8d14-48af-b3ea-6482dce86832
 md"""
-We see that our image is an $(size(img_data, 1)) × $(size(img_data, 2)) array of $(eltype(img_data)) data. This can be visualized in the same way as our previous heatmap example, which we will show next.
+Here is some information about our image:
+"""
 
-!!! tip "Todo"
-    We use the `scale = false` keyword in our `fits` call to preserve the original data type specified in the `BITPIX` header card. For more, see <documentation coming soon>.
+# ╔═╡ d413263f-8896-4060-bc6a-e849ef683a19
+size(img) # NAXIS1 x NAXIS2 dimension
+
+# ╔═╡ 798590cd-b1c9-44d7-90b7-71d470abce5a
+eltype(img) # Data type of each pixel in our image
+
+# ╔═╡ 3a5ec935-41f8-4845-bfeb-d6e5836353bf
+header(img) # The stored header file
+
+# ╔═╡ fc3001ac-dd95-467a-9be8-a581d9fef628
+img.data # View the underlying data as a matrix
+
+# ╔═╡ a9548ae8-ab0e-42be-825d-8146b95196a2
+md"""
+Under the hood, AstroImages.jl calls [FITSFiles.jl](https://juliaastro.org/FITSFiles) to load our data for us. By default, this takes the first HDU to be our image data, but we could also load all HDUs by passing `:` to the above `load` function:
+"""
+
+# ╔═╡ 4cb7062f-4d86-4ac6-884e-cd7546ef5b90
+load(img_file, :)
+
+# ╔═╡ ee2c3044-9c44-4a49-a0c8-2ff77ee5a83d
+md"""
+We see that in addition to the first block holding our image data, we also have a second block containing table data. AstroImages.jl is specialized for image data, so for more information on accessing the table information, it is recommended to use the lower-level FITSFiles.jl package as shown in the first tutorial ([learn.JuliaAstro > Working with FITS tables](/tutorials/fits-tables/)).
 """
 
 # ╔═╡ b3c6961a-b8a6-4597-aaf7-a97cae793670
@@ -308,16 +166,10 @@ md"""
 Since this is just plain array data instead of tabular / `DataFrame` data, we will use Makie.jl without the AlgebraOfGraphics.jl framework:
 """
 
-# ╔═╡ 3b8c84de-56f5-4d1e-9a63-a7abbc33bd55
-plot(img_data; colormap = :magma)
-
 # ╔═╡ 83af6865-8c5a-436c-825f-f57f5bec7ec3
 md"""
 Alternatively, we can load the fits array and visualize it directly with AstroImages.jl:
 """
-
-# ╔═╡ 3b813a3a-b24d-4801-bd1a-d7ac3106518b
-img = load(download("http://data.astropy.org/tutorials/FITS-images/HorseHead.fits"))
 
 # ╔═╡ 3158e659-3efa-4a64-a548-7076a3a03301
 md"""
@@ -369,6 +221,34 @@ and access the raw underlying data anytime:
 
 # ╔═╡ 227ea834-faa9-4540-9e0b-b9c87cd19b45
 img.data
+
+# ╔═╡ 5dd43020-71e9-47e7-9b57-295e57a98bce
+md"""
+### Plotting with Makie.jl
+
+Makie.jl is a modern plotting ecosystem written in pure Julia. Its [set of backends](https://docs.makie.org/stable/explanations/backends/backends#What-is-a-backend) allows us to produce plots for a wide range of contexts. For this tutorial, we will use the CairoMakie.jl backend to produce publication-quality vector graphic plots.
+
+To start, we will pass the histogram object `h` directly to Makie, which its `plot` command knows how to handle:
+"""
+
+# ╔═╡ 02eaf214-7238-45b3-9674-c1b7f1b7d10e
+let
+    fig, ax, p = plot(
+        img;
+        colorrange = (1, 10_000),
+        colorscale = log10,
+        colormap = :cividis,
+    )
+
+    Colorbar(
+        fig[1, 2], p;
+        ticks = [1, 3, 6, 500, 10_000],
+        minorticksvisible = true,
+        minorticks = IntervalsBetween(9),
+    )
+
+    fig
+end
 
 # ╔═╡ 9b1ec23d-e773-4893-83ba-c465fd510778
 md"""
@@ -525,9 +405,10 @@ This notebook is modified from <https://learn.astropy.org/tutorials/FITS-images.
 _Original authors: Lia Corrales, Kris Stern, Stephanie T. Douglas, Kelle Cruz, Lúthien Liu, Zihao Chen, Saima Siddiqui_
 
 !!! tip "Learning goals"
-    - Customize a 2D histogram with image data.
-    - Stack several images into a single image (Todo).
-    - Write image data to a FITS file (Todo).
+    1. Open FITS files and load image data
+    1. Make a 2D histogram with image data
+    1. Stack several images into a single image
+    1. Write image data to a FITS fil
 
 $(keywords())
 
@@ -540,37 +421,21 @@ $(keywords())
 # ╟─91f00e98-e69c-4435-b9d0-10d30006efef
 # ╟─a6e33cf8-1fe3-4810-a66b-adc07166871e
 # ╠═61c0bf34-302b-4732-a44d-4c2da611eb74
-# ╟─f87da6ab-c718-47c3-bedc-78cd025b40e6
-# ╟─3b974213-d2a5-4528-b27b-d4c608319213
-# ╠═bfbf0366-1115-4e96-beb1-0b012dbd8a18
-# ╠═2472e2a8-27b6-4f18-9dce-d0ef12d6c257
-# ╟─ae04bf64-0ea2-4da2-b6ae-a86f11a786b8
-# ╟─c26d8744-0026-4f43-b056-1c94ee958f8b
-# ╠═430611bb-6caf-4b38-837d-fc05173d4f00
-# ╟─515e570b-d140-4d66-845e-cae7b4cecd05
-# ╠═bb11ae83-b919-4511-86cf-51baf77d89a3
-# ╟─7401caa5-c891-4306-bb3c-a4a9cab7012b
-# ╠═d031ddf9-ee84-46b2-b387-1ec99a2ca79b
-# ╟─c4374601-9c65-4bd2-b2c0-3e83597395bf
-# ╟─5dd43020-71e9-47e7-9b57-295e57a98bce
-# ╠═02eaf214-7238-45b3-9674-c1b7f1b7d10e
 # ╟─74f155a2-fe8a-404f-b0c6-7a4f2724c408
-# ╟─88f9fe70-44d0-469c-8472-8def72b7c3d0
-# ╠═f667c0c9-b52c-4fca-9a74-f967c25279d8
-# ╟─414b0415-e784-4089-a683-a20ec15ee44f
-# ╠═235242b0-a8c6-4627-9b2c-fc14f705c686
-# ╟─5ea5f0ee-ae78-4f35-8900-011416c7d508
-# ╟─f7aca318-a535-4a86-8c29-92d90db96271
 # ╟─0598bfc5-0900-4b04-ad84-9638b89869b9
-# ╟─ad35dd30-40f5-4bd7-b1d6-b81a226e85ab
-# ╠═1e05c329-a82a-4ed8-ba02-11e155539059
-# ╟─eaf39dba-e724-4eac-9a14-28fcd179efc7
-# ╠═798dbe98-c2d5-4dd2-b624-f5df795ef470
-# ╟─a9ec676e-8d14-48af-b3ea-6482dce86832
-# ╟─b3c6961a-b8a6-4597-aaf7-a97cae793670
-# ╠═3b8c84de-56f5-4d1e-9a63-a7abbc33bd55
-# ╟─83af6865-8c5a-436c-825f-f57f5bec7ec3
+# ╠═622ae936-36d2-4154-a82b-e8fe33773f92
 # ╠═3b813a3a-b24d-4801-bd1a-d7ac3106518b
+# ╟─d9881179-49b8-40af-8aa0-233d7bb1496c
+# ╟─a9ec676e-8d14-48af-b3ea-6482dce86832
+# ╠═d413263f-8896-4060-bc6a-e849ef683a19
+# ╠═798590cd-b1c9-44d7-90b7-71d470abce5a
+# ╠═3a5ec935-41f8-4845-bfeb-d6e5836353bf
+# ╠═fc3001ac-dd95-467a-9be8-a581d9fef628
+# ╟─a9548ae8-ab0e-42be-825d-8146b95196a2
+# ╠═4cb7062f-4d86-4ac6-884e-cd7546ef5b90
+# ╟─ee2c3044-9c44-4a49-a0c8-2ff77ee5a83d
+# ╟─b3c6961a-b8a6-4597-aaf7-a97cae793670
+# ╟─83af6865-8c5a-436c-825f-f57f5bec7ec3
 # ╟─3158e659-3efa-4a64-a548-7076a3a03301
 # ╠═010ddf8b-7355-41cf-8d92-0d217e382978
 # ╠═23d0d42f-77e2-4b21-a350-f9f08eabbd22
@@ -582,6 +447,8 @@ $(keywords())
 # ╠═0fa6d28a-4608-444b-ae2c-5845ec8a21b1
 # ╟─997258a0-39ca-4e5f-a143-f3bdbefd265f
 # ╠═227ea834-faa9-4540-9e0b-b9c87cd19b45
+# ╟─5dd43020-71e9-47e7-9b57-295e57a98bce
+# ╠═02eaf214-7238-45b3-9674-c1b7f1b7d10e
 # ╟─9b1ec23d-e773-4893-83ba-c465fd510778
 # ╠═7c3e5faf-6f80-4413-a72e-bfce5a59fe07
 # ╟─7a582e8c-03fb-406e-83ef-9b15920fb6ba
